aafdc6ca08b22849e1169cba3ceffbf23f7607e0
SabbirxPro <proxsabbir@gmail.com>
2024-08-18 14:41:49 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = "24260604"
+API_HASH = "4a146cad2009abcf5707b97ac98656ae"
+BOT_TOKEN = "7123971690:AAEJUCSTE25Yi_YtB-F9feWmtEGHSgWmEMU"
+OWNER_ID = 6769376828
+MONGODB_CONNECTION_STRING = "mongodb+srv://proxsabbir:proxsabbir@proxmirror.nzth76r.mongodb.net/?retryWrites=true&w=majority&appName=ProXmirror"
+LOG_GROUP = "-1002238939229"
+FORCESUB = "bdbots"
